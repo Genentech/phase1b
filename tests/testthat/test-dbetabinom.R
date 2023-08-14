4342b5a66e19685957539bfe845b380137e77518
Audrey Yeo <audrey.yeo@roche.com>
2023-08-14 17:56:34 +0200

    removed rounding, not needed

--- tests/testthat/test-dbetabinom.R
+++ tests/testthat/test-dbetabinom.R
@@ -1,5 +1,4 @@
 test_that("the dbetabinom density for every x support is between 0 and 1", {
-  results <- round(dbetabinom(10, 20, 0.7, 2), digits = 2)
-  expected <- 0.04
+  results <- dbetabinom(10, 20, 0.7, 2)
   expect_number(results, lower = 0, upper = 1)
 })
