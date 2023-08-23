a69cc3ec25c2ad1e315319668c450f83aebd312c
Audrey Yeo <audrey.yeo@roche.com>
2023-08-23 18:05:31 +0200

    for dbetabinom and dbetabinomMix, created/edited : test functions and example files

--- tests/testthat/test-dbetabinom.R
+++ tests/testthat/test-dbetabinom.R
@@ -1,25 +1,49 @@
-test_that("the dbetabinom density for every x support is between 0 and 1", {
+# dbetabinom ----
+
+test_that("the dbetabinom density for every x support is between 0 and 1.", {
   results <- dbetabinom(10, 20, 0.7, 2)
   expect_number(results, lower = 0, upper = 1)
 })
 
-test_that("the sum of the dbetabinom density for all x is 1", {
+test_that("the sum of the dbetabinom density for all x is 1.", {
   result <- sum(dbetabinom(0:10, 10, 1, 1))
   expect_equal(result, 1)
 })
 
-test_that("the beta mixture has a numeric result between 0 and 1", {
-  result <- dbetabinomMix(10, 20,
-    matrix(c(1, 2), ncol = 2, nrow = 1),
-    weights = c(0.2, 0.8)
+test_that("Beta binomial density has a result of 0.04286893, given x = 2; m = 29; a = 0.2; b = 0.4.", {
+  result <- dbetabinom(x = 2, m = 29, a = 0.2, b = 0.4)
+  expect_equal(result, 0.04286893)
+})
+
+# dbetabinomMix ----
+
+test_that("the beta mixture has a result between 0 and 1.", {
+  result <- dbetabinomMix(
+    x = 2, m = 29,
+    par = rbind(c(0.2, 0.4)),
+    weights = 1
   )
   expect_numeric(result, lower = 0, upper = 1, finite = TRUE)
 })
 
-test_that("the sum of the dbetabinom density for all x is 1", {
+test_that("the beta mixture density has a result of 0.04286893.", {
+  result <- dbetabinomMix(
+    x = 2, m = 29,
+    par = rbind(c(0.2, 0.4)),
+    weights = 1
+  )
+  expect_equal(result, 0.04286893, tolerance = 1e-6)
+})
+
+test_that("the sum of the beta mixturedensity for all x is 1.", {
   result <- sum(dbetabinomMix(0:20, 20,
     matrix(c(1, 2), ncol = 2, nrow = 1),
     weights = c(0.2, 0.8)
   ))
   expect_equal(result, 1)
+})
+
+test_that("Beta mixture density has the correct result", {
+  result <- dbetabinomMix(x = 2, m = 29, par = rbind(c(0.2, 0.4), c(1, 1)), weights = c(0.6, 0.4))
+  expect_equal(result, 0.03905469, tolerance = 1e-6)
 })
