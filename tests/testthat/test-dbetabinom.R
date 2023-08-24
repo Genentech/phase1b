66e9d59ee2940f15939e9320ae36bb7692222e33
Audrey Yeo <comptesaudrey@gmail.com>
2023-08-24 12:03:46 +0200

    Update tests/testthat/test-dbetabinom.R
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>

--- tests/testthat/test-dbetabinom.R
+++ tests/testthat/test-dbetabinom.R
@@ -1,6 +1,6 @@
 # dbetabinom ----
 
-test_that("the dbetabinom density for every x support is between 0 and 1.", {
+test_that("the dbetabinom density for every x support is between 0 and 1", {
   results <- dbetabinom(10, 20, 0.7, 2)
   expect_number(results, lower = 0, upper = 1)
 })
