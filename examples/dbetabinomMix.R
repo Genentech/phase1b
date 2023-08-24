b7bfdb9c418a455e251475ae66b58d9ce4fc6718
Audrey Yeo <audrey.yeo@roche.com>
2023-08-24 12:08:01 +0200

    periods

--- examples/dbetabinomMix.R
+++ examples/dbetabinomMix.R
@@ -4,7 +4,7 @@ dbetabinomMix(
   x = 2, m = 29, par = rbind(c(0.2, 0.4), c(1, 1)),
   weights = c(0.6, 0.4)
 )
-
+# x could be a vector
 dbetabinomMix(
   x = 1:28, m = 29, par = rbind(c(0.2, 0.4), c(1, 1)),
   weights = c(0.6, 0.4)
--- tests/testthat/test-dbetabinom.R
+++ tests/testthat/test-dbetabinom.R
@@ -5,7 +5,7 @@ test_that("the dbetabinom density for every x support is between 0 and 1", {
   expect_number(results, lower = 0, upper = 1)
 })
 
-test_that("the sum of the dbetabinom density for all x is 1.", {
+test_that("the sum of the dbetabinom density for all x is 1", {
   result <- sum(dbetabinom(0:10, 10, 1, 1))
   expect_equal(result, 1)
 })
@@ -17,7 +17,7 @@ test_that("Beta binomial density has correct numeric result for specific inputs"
 
 # dbetabinomMix ----
 
-test_that("the beta mixture has a result between 0 and 1.", {
+test_that("the beta mixture has a result between 0 and 1", {
   result <- dbetabinomMix(
     x = 2, m = 29,
     par = rbind(c(0.2, 0.4)),
@@ -26,7 +26,7 @@ test_that("the beta mixture has a result between 0 and 1.", {
   expect_numeric(result, lower = 0, upper = 1, finite = TRUE)
 })
 
-test_that("the beta mixture density has a result of 0.04286893.", {
+test_that("the beta mixture density has the correct numeric result", {
   result <- dbetabinomMix(
     x = 2, m = 29,
     par = rbind(c(0.2, 0.4)),
@@ -43,7 +43,7 @@ test_that("the sum of the beta mixture density for all x is 1", {
   expect_equal(result, 1)
 })
 
-test_that("Beta mixture density has the correct result.", {
+test_that("Beta mixture density has the correct numeric result", {
   result <- dbetabinomMix(x = 2, m = 29, par = rbind(c(0.2, 0.4), c(1, 1)), weights = c(0.6, 0.4))
   expect_equal(result, 0.03905469, tolerance = 1e-6)
 })
