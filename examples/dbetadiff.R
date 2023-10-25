9c6ad6d374974c281de01526aa1a6c11eca757c5
Audrey Yeo <comptesaudrey@gmail.com>
2023-10-25 16:16:05 +0200

    15 betadiff (#19)
    * cleaning up dbetabinom
    
    * removed rounding, not needed
    
    * rmarkdown language and assert for dbetabinom
    
    * changed x and m to number instead of numeric
    
    * test
    
    * pbetaMix function and test
    
    * added new tests and changed documentation
    
    * upper case for test_that comments and more tests for qbeta
    
    * test_thats for pbetaMix and qbetaMix
    
    * tidy syntax and last checks
    
    * tidy syntax and last checks
    
    * Update R/dbetabinom.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * [skip actions] Roxygen Man Pages Auto Update
    
    * Update R/dbetabinom.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update R/dbetabinom.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update R/dbetabinom.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update R/dbetabinom.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update R/dbetabinom.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update R/dbetabinom.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update R/dbetabinom.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update examples/pbetaMix.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * p and q inputs
    
    * q,p changes
    
    * Update R/dbetabinom.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * [skip actions] Roxygen Man Pages Auto Update
    
    * documentation to reflect p and q changes
    
    * amending qt
    
    * intro x-> q
    
    * amented postprob file
    
    * amended postprob file
    
    * fixed mistake on x
    
    * qbetaMix was in postprobDist
    
    * trying to find R CMD check error
    
    * [skip actions] Roxygen Man Pages Auto Update
    
    * manual of ocPostprobDist
    
    * fixing roxygen type to p
    
    * see if this fixes the CMD check
    
    * see if fixes CMD check error
    
    * xi removed
    
    * example post prob works
    
    * clean
    
    * assert_number
    
    * in postprobDist, I changed p-> q
    
    * change p to q
    
    * postprobold
    
    * postprobOld
    
    * clean post prob
    
    * nice titles
    
    * documentations
    
    * clean
    
    * clean
    
    * clean
    
    * clean
    
    * clean
    
    * clean
    
    * some examples and tests
    
    * clean
    
    * fix R CMD check errors
    
    * fix R CMD check errors for PR
    
    * clean
    
    * clean
    
    * clean
    
    * Auto stash before checking out "origin/main"
    
    * clean
    
    * clean
    
    * clean
    
    * clean
    
    * [skip actions] Roxygen Man Pages Auto Update
    
    * clean
    
    * Update R/betadiff.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * [skip actions] Roxygen Man Pages Auto Update
    
    * clean
    
    * clean
    
    * remove get_
    
    * eps argument
    
    * Update tests/testthat/test-betadiff.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * clean
    
    * Update R/betadiff.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * clean
    
    * clean
    
    * just added stats::integrate in the documentation
    
    * clean
    
    * stats::integrate()
    
    * pr feedback
    
    * Update R/betadiff.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * clean
    
    * clean
    
    ---------
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    Co-authored-by: 27856297+dependabot-preview[bot]@users.noreply.github.com <27856297+dependabot-preview[bot]@users.noreply.github.com>

---
+++ examples/dbetadiff.R
@@ -0,0 +1,29 @@
+# The following examples use these parameters:
+parX <- c(1, 52)
+parY <- c(5.5, 20.5)
+
+# The difference between Control and Treatment is denoted as z.
+z <- seq(from = -1, to = 1, length = 100)
+plot(z, dbetadiff(z, parY = parY, parX = parX),
+  type = "l"
+)
+
+# Calculate probability of Go, if difference was at least 15%.
+test <- integrate(
+  f = dbetadiff,
+  parY = parY,
+  parX = parX,
+  lower = 0.15,
+  upper = 1
+)
+str(test)
+test$value
+
+# Calculate probability of Stop, if difference was at most 50%.
+integrate(
+  f = dbetadiff,
+  parY = parY,
+  parX = parX,
+  lower = -1,
+  upper = 0.5
+)
