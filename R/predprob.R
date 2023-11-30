6141fbd5ae654059893fa6181950a6ce24b36ab1
Audrey Yeo <audrey.yeo@roche.com>
2023-11-30 11:15:30 +0100

    clean

--- R/predprob.R
+++ R/predprob.R
@@ -17,14 +17,13 @@ NULL
 #' `pp = sum over i: Pr(Y=i|x,n)*I{Pr(P_E > p|x,Y=i)>=thetaT}`
 #' where `Y` is the number of future responses in the treatment group and `x` is
 #' the current number of responses in the treatment group out of n.
-#' The prior of `Y` is `P_E ~ beta(a, b)` has prior `beta(1,1)` or other if beta mixture prior
-#' specified.
+#' The prior for the response rate in the experimental arm is `P_E ~ beta(a, b)`.
 #'
 #' A table with the following contents will be included in the return output :
-#' `i`: `Y = i`, number of future successes in `Nmax-n` subjects.
-#' `density`: `Pr(Y = i|x)` using beta-(mixture)-binomial distribution.
-#' `posterior`: `Pr(P_E > p | x, Y=i)` using beta posterior.
-#' `result_thetaT`: indicator `I(b>thetaT)`.
+#' - `i`: `Y = i`, number of future successes in `Nmax-n` subjects.
+#' - `density`: `Pr(Y = i|x)` using beta-(mixture)-binomial distribution.
+#' - `posterior`: `Pr(P_E > p | x, Y=i)` using beta posterior.
+#' - `result_thetaT`: indicator `I(b>thetaT)`.
 #'
 #' @typed x : number
 #'  number of successes.
@@ -38,12 +37,9 @@ NULL
 #'  threshold on the probability to be above p.
 #' @typed parE : numeric
 #'  the beta parameters matrix, with K rows and 2 columns,
-#'  corresponding to the beta parameters of the K components. Default is a
-#'  uniform prior.
+#'  corresponding to the beta parameters of the K components.
 #' @typed weights : numeric
-#'  the mixture weights of the beta mixture prior. Default are
-#'  equal weights across mixture components. Weights can exceed 1 which will be
-#'  normalised in `[h_getBetamixPost()]`.
+#'  the mixture weights of the beta mixture prior.
 #' @return A `list` is returned with names `result` for predictive probability and
 #'  `table` of numeric values with counts, densities, posterior and result of indicator function.
 #'
@@ -67,23 +63,23 @@ predprob <- function(x, n, Nmax, p, thetaT, parE = c(1, 1),
   }
   betamixPost <- h_getBetamixPost(x = x, n = n, par = parE, weights = weights)
 
-  py <- with(
+  density <- with(
     betamixPost,
     dbetabinomMix(x = 0:m, m = m, par = par, weights = weights)
   )
-  # posterior probabilities to be above threshold p
-  posterior <- postprob(x = x + c(0:m), n = Nmax, p = p, parE = parE, weights = weights)
   assert_numeric(density, lower = 0, upper = 1, finite = TRUE, any.missing = FALSE)
   assert_numeric(posterior, lower = 0, upper = 1, finite = TRUE, any.missing = FALSE)
   assert_number(thetaT, lower = 0, upper = 1, finite = TRUE)
+  # posterior probabilities to be above threshold p
+  posterior <- postprob(x = x + c(0:m), n = Nmax, p = p, parE = parE, weights = weights)
   list(
     result = sum(density * (posterior > thetaT)),
     table = data.frame(
       counts = c(0:m),
       cumul_counts = n + (0:m),
       density = round(density, 4),
-      posterior = round(posterior, 4),
-      result_theta = ifelse((posterior > thetaT), 1, 0)
+      posterior = posterior,
+      success = (posterior > thetaT)
     )
   )
 }
