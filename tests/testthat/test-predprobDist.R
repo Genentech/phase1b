a888d482c7d39e8e9e1801ec543cce8b72828d5b
Audrey Yeo <audrey.yeo@roche.com>
2023-12-06 11:36:26 +0100

    helper functions x2

--- tests/testthat/test-predprobDist.R
+++ tests/testthat/test-predprobDist.R
@@ -16,15 +16,48 @@ parE <- t(parE)
 weights <- rep(1, nrow(parE))
 parS <- t(parS)
 weightsS <- rep(1, nrow(parS))
-
-h_get_predproblist_control(x = x,
-                           n = Nmax,
-                           delta = delta,
-                           relativeDelta = relativeDelta,
-                           parE = parE,
-                           weights = weights,
-                           parS = parS,
-                           weightsS = weightsS,
-                           thetaT = thetaT,
-                           density = 0.77,
-                           mE = 17)
+h_get_predproblist_control <- function(x,
+                                       n,
+                                       delta,
+                                       relativeDelta,
+                                       parE,
+                                       weights,
+                                       parS,
+                                       weightsS,
+                                       thetaT,
+                                       density,
+                                       mE) {
+  posterior <- postprobDist(
+    x = x + c(0:mE),
+    n = Nmax,
+    delta = delta,
+    relativeDelta = relativeDelta,
+    parE = parE,
+    weights = weights,
+    parS = parS,
+    weightsS = weightsS
+  )
+  list(
+    result = sum(density * (posterior > thetaT)),
+    table = data.frame(
+      counts = c(0:mE),
+      cumul_counts = n + (0:mE),
+      density = round(density, 4),
+      posterior = posterior,
+      success = (posterior > thetaT)
+    )
+  )
+}
+h_get_predproblist_control(
+  x = x,
+  n = Nmax,
+  delta = delta,
+  relativeDelta = relativeDelta,
+  parE = parE,
+  weights = weights,
+  parS = parS,
+  weightsS = weightsS,
+  thetaT = thetaT,
+  density = 0.77,
+  mE = 17
+)
