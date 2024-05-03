10ea9d1a467d630268c3e40ff5419d03bbd34da6
Audrey Yeo <comptesaudrey@gmail.com>
2024-05-03 21:56:28 +0200

    85_ocRctPostprobDist (#86)
    Co-authored-by: 27856297+dependabot-preview[bot]@users.noreply.github.com <27856297+dependabot-preview[bot]@users.noreply.github.com>
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>

--- R/ocPostprobDist.R
+++ R/ocPostprobDist.R
@@ -181,7 +181,7 @@ ocPostprobDist <- function(nnE,
   assert_number(tU, lower = 0, upper = 1)
   assert_numeric(parE, lower = 0, finite = TRUE, any.missing = FALSE)
   assert_numeric(parS, lower = 0, finite = TRUE, any.missing = FALSE)
-  assert_number(sim, lower = 100, finite = TRUE)
+  assert_number(sim, lower = 1, finite = TRUE)
   assert_flag(wiggle)
   assert_numeric(nnF, min.len = 0, any.missing = FALSE)
 
@@ -207,6 +207,7 @@ ocPostprobDist <- function(nnE,
       nnrE <- nnr$nnrE
       nnrF <- nnr$nnrF
     } else {
+      dist <- NULL
       nnrE <- nnE
       nnrF <- nnF
     }
