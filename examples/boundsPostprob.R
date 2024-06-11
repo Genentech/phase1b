150f2438ba693f1b18ab3ad2157a948777fce10a
Audrey Yeo <comptesaudrey@gmail.com>
2024-06-11 14:05:26 +0200

    Update examples/boundsPostprob.R
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>

--- examples/boundsPostprob.R
+++ examples/boundsPostprob.R
@@ -1,6 +1,6 @@
 # 40 pts trial with interim looks after each 10 pts.,
 # Efficacy decision if more than 60% probability to be above 20% ORR,
-# Futility decision if less than 60% probability to be below 20% ORR,
+# Futility decision if more than 60% probability to be below 20% ORR,
 # with uniform prior (i.e. beta(1, 1)) on the ORR:
 boundsPostprob(
   nvec = c(10, 20, 30, 40),
