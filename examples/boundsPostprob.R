0b2e6adec89eaf509c76275b7f6e72502a10573c
Audrey Yeo <comptesaudrey@gmail.com>
2024-06-27 09:45:09 +0200

    changed params in bounds example

--- examples/boundsPostprob.R
+++ examples/boundsPostprob.R
@@ -1,13 +1,13 @@
 # 40 pts trial with interim looks after each 10 pts.,
-# Efficacy decision if more than 60% probability to be above 20% ORR,
+# Efficacy decision if more than 80% probability to be above 20% ORR,
 # Futility decision if more than 60% probability to be below 20% ORR,
 # with uniform prior (i.e. beta(1, 1)) on the ORR:
 boundsPostprob(
   nvec = c(10, 20, 30, 40),
   p0 = 0.20,
   p1 = 0.20,
-  tL = 0.20,
-  tU = 0.20,
+  tL = 0.60,
+  tU = 0.80,
   a = 1,
   b = 1
 )
