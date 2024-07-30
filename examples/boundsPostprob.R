6dd1b824efbe1d80f1e0d89a421de078259d5216
Audrey Yeo <comptesaudrey@gmail.com>
2024-07-30 18:20:38 +0200

    115 make website live (#116)
    Co-authored-by: 27856297+dependabot-preview[bot]@users.noreply.github.com <27856297+dependabot-preview[bot]@users.noreply.github.com>

--- examples/boundsPostprob.R
+++ examples/boundsPostprob.R
@@ -1,10 +1,8 @@
-## 40 pts trial with interim looks after each 10 pts.,
-## efficacy decision if more than 90% probability to be above 20% ORR,
-## futility decision if less than 10% probability to be above 20% ORR,
-## with uniform prior (i.e. beta(1, 1)) on the ORR:
+# 40 pts trial with interim looks after each 10 pts.,
+# Efficacy decision if more than 60% probability to be above 20% ORR,
+# Futility decision if less than 60% probability to be below 20% ORR,
+# with uniform prior (i.e. beta(1, 1)) on the ORR:
 boundsPostprob(
-  nvec = c(10, 20, 30, 40), p0 = 0.20,
-  tL = 0.10, tU = 0.90, a = 1, b = 1
+  nvec = c(10, 20, 30, 40), p0 = 0.20, p1 = 0.2,
+  tL = 0.60, tU = 0.60, a = 1, b = 1
 )
-## From this we see e.g. that at the third IA at 30 pts, we would stop for futility
-## if 5 or less patients responded, and for efficacy if 9 or more pts responded.
