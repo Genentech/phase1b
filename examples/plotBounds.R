bb749b91dedab121f04d1d2d0f142d1afa55e831
Audrey Yeo <comptesaudrey@gmail.com>
2024-10-11 13:49:23 +0200

    clean

--- examples/plotBounds.R
+++ examples/plotBounds.R
@@ -1,1 +1,9 @@
 # examples
+plotBounds(boundsPredprob(
+  nvec = c(10, 20, 30, 40), p = 0.20, tT = 0.80,
+  phiL = 0.10, phiU = 0.90, a = 1, b = 1
+), yt = "x")
+plotBounds(boundsPredprob(
+  nvec = c(10, 20, 30, 40), p = 0.20, tT = 0.80,
+  phiL = 0.10, phiU = 0.90, a = 1, b = 1
+), yt = "p")
