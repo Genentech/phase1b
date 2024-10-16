855dc8ec2957a5c7b5e76f8c4b8d90adde8a2513
Audrey Yeo <comptesaudrey@gmail.com>
2024-10-16 14:35:00 +0200

    added plotBounds(boundsPredprob()) examples

--- examples/plotBounds.R
+++ examples/plotBounds.R
@@ -1,1 +1,9 @@
 # examples
+plotBounds(boundsPredprob(
+  looks = c(10, 20, 30, 40), p0 = 0.20, tT = 0.80,
+  phiL = 0.10, phiU = 0.90,
+), yt = "x")
+plotBounds(boundsPredprob(
+  looks = c(10, 20, 30, 40), p0 = 0.20, tT = 0.80,
+  phiL = 0.10, phiU = 0.90,
+), yt = "p")
