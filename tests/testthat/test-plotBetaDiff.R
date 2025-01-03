c475a98d816fa45dda6bddad34a8ab401625671d
Audrey Yeo <comptesaudrey@gmail.com>
2025-01-03 14:59:31 +0100

    for PR review before .svg files for test

---
+++ tests/testthat/test-plotBetaDiff.R
@@ -0,0 +1,22 @@
+# plotBetaDiff
+test_that("plotBetaDiff works as expected", {
+  result <- plotBetaDiff(
+    parY = c(1, 1),
+    parX = c(6, 10),
+    Go_cut = 0.3,
+    Stop_cut = 0.1, # below a difference of 10%, is an unsuccesful trial
+    shade = TRUE,
+    note = TRUE
+  )
+  result <- plotBetaDiff(
+    parY = c(2, 3),
+    parX = c(4, 10),
+    Go_cut = 0.3,
+    Stop_cut = 0.1, # below a difference of 10%, is an unsuccesful trial
+    shade = TRUE,
+    note = TRUE
+  )
+  vdiffr::expect_doppelganger("", result)
+  vdiffr::expect_doppelganger("", result)
+
+})
