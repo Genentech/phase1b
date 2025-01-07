e500868b3d2d5ef5596175d338b493c1d6a867dc
Audrey Yeo <comptesaudrey@gmail.com>
2025-01-07 10:50:31 +0100

    some first workings

--- examples/plotOc.R
+++ examples/plotOc.R
@@ -1,9 +1,46 @@
 # get operating character result from oc.postprob
 
-res1 <- ocPostprob(
+oc <- ocPostprob(
   nnE = c(10, 20, 30), truep = 0.4, p0 = 0.2,
-  p1 = 0.3, tL = 0.6, tU = 0.8, parE = c(1, 1), sim = 50000
+  p1 = 0.3, tL = 0.6, tU = 0.8, parE = c(1, 1), sim = 100, wiggle = FALSE
 )
-res1$oc
+oc$oc
+# plotOc(oc)
 
-plotOc(res1)
+
+oc <- ocPostprob(
+  nnE = c(10, 20, 30), truep = 0.4, p0 = 0.2,
+  p1 = 0.3, tL = 0.6, tU = 0.8, parE = c(1, 1), sim = 100, wiggle = TRUE
+)
+oc$oc
+
+data <- data.frame(Decision = as.factor(oc$Decision),
+           Looks = oc$SampleSize)
+
+table(data$Decision, data$Looks)
+
+data %>% group_by(Decision, Looks) %>% summarise(n = n())
+
+data %>% group_by(SampleSize) %>% summarise(n = n())
+
+ggplot(dat, aes(x = SampleSize, y = Decision)) +
+  geom_bar(stat = "identity")
+
+data %>% group_by(Looks, Decision) %>% summarise(tot = n())
+
+
+table(oc$Decision, oc$SampleSize) / oc$params$sim
+table(oc$Decision, oc$SampleSize)
+
+oc$Decision <- ifelse(oc$Decision == TRUE, "GO",
+                      ifelse(oc$Decision == FALSE, "Stop", "Grey"))
+
+oc$Decision
+
+data.frame(looks = c(res1$wiggled_nnrE))
+
+table(oc$Decision, oc$SampleSize)
+
+tt = data.frame(Decision = res$Decision,
+                look = res$SampleSize)
+tt %>% group_by(look) %>% summarise(success = per())
