8c9926a3d29b532fc3b84142ce1e78ed78bd3055
Audrey Yeo <comptesaudrey@gmail.com>
2025-01-27 19:05:59 +0100

    clean

--- tests/testthat/test-plotBetaDiff.R
+++ tests/testthat/test-plotBetaDiff.R
@@ -1,21 +1,23 @@
 # plotBetaDiff
 test_that("plotBetaDiff works as expected", {
-  result <- plotBetaDiff(
-    parY = c(1, 1),
-    parX = c(6, 10),
+  parX <- c(1, 52) # prior  parameters of experimental arm
+  parY <- c(5.5, 20.5) # prior  parameters of control or SOC
+  result1 <- plotBetaDiff(
+    parY = parY,
+    parX = parX,
     Go_cut = 0.3,
-    Stop_cut = 0.1, # below a difference of 10%, is an unsuccesful trial
+    Stop_cut = 0.1, # below a difference of 10%, is an unsuccessful trial
     shade = TRUE,
     note = TRUE
   )
-  result <- plotBetaDiff(
-    parY = c(2, 3),
-    parX = c(4, 10),
+  result2 <- plotBetaDiff(
+    parY = c(1, 1), # prior  parameters for experimental arm
+    parX = c(1, 1), # prior parameters for control or SOC arm
     Go_cut = 0.3,
-    Stop_cut = 0.1, # below a difference of 10%, is an unsuccesful trial
+    Stop_cut = 0.1, # below a difference of 10%, is an unsuccessful trial
     shade = TRUE,
     note = TRUE
   )
-  vdiffr::expect_doppelganger("", result)
-  vdiffr::expect_doppelganger("", result)
+  vdiffr::expect_doppelganger("Plot of distibution of difference of two arms", result1)
+  vdiffr::expect_doppelganger("Plot of distibution of difference of two arms with beta mixture", result2)
 })
