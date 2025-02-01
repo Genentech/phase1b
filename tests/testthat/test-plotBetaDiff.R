10657827b4f3310c4996ff71e2159fbdd0679a62
Audrey Yeo <comptesaudrey@gmail.com>
2025-02-01 20:09:02 +0100

    small cases arguments

--- tests/testthat/test-plotBetaDiff.R
+++ tests/testthat/test-plotBetaDiff.R
@@ -3,18 +3,18 @@ test_that("plotBetaDiff works as expected", {
   parX <- c(1, 52) # prior  parameters of experimental arm
   parY <- c(5.5, 20.5) # prior  parameters of control or SOC
   result1 <- plotBetaDiff(
-    parY = parY,
-    parX = parX,
-    Go_cut = 0.3,
-    Stop_cut = 0.1, # below a difference of 10%, is an unsuccessful trial
+    parE = parY,
+    parS = parX,
+    go_cut = 0.3,
+    stop_cut = 0.1, # below a difference of 10%, is an unsuccessful trial
     shade = TRUE,
     note = TRUE
   )
   result2 <- plotBetaDiff(
-    parY = c(1, 1), # prior  parameters for experimental arm
-    parX = c(1, 1), # prior parameters for control or SOC arm
-    Go_cut = 0.3,
-    Stop_cut = 0.1, # below a difference of 10%, is an unsuccessful trial
+    parE = c(1, 1), # prior  parameters for experimental arm
+    parS = c(1, 1), # prior parameters for control or SOC arm
+    go_cut = 0.3,
+    stop_cut = 0.1, # below a difference of 10%, is an unsuccessful trial
     shade = TRUE,
     note = TRUE
   )
