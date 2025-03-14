8dccc19978f1385b9e0d2db054bdecf080bc96f0
Audrey Yeo <comptesaudrey@gmail.com>
2025-03-14 09:13:36 +0100

    lintr improvements

--- tests/testthat/test-plotOc.R
+++ tests/testthat/test-plotOc.R
@@ -234,8 +234,12 @@ test_that("plotOc gives expected results for `ocPostprob` and `ocPredprob`", {
     all_looks = res5$Looks,
     wiggle_status = res5$params$wiggle
   )
-  vdiffr::expect_doppelganger(title = "plot of simulation result for single arm posterior probability", fig = result1)
-  vdiffr::expect_doppelganger(title = "plot of simulation result for single arm posterior predictive probability", fig = result2)
+  vdiffr::expect_doppelganger(
+    title = "plot of simulation result for single arm posterior probability",
+    fig = result1)
+  vdiffr::expect_doppelganger(
+    title = "plot of simulation result for single arm posterior predictive probability",
+    fig = result2)
 })
 
 test_that("plotOc gives expected results for `ocPredprobDist` with different relativeDelta status", {
@@ -287,6 +291,10 @@ test_that("plotOc gives expected results for `ocPredprobDist` with different rel
     all_looks = res8$Looks,
     wiggle_status = res8$params$wiggle
   )
-  vdiffr::expect_doppelganger(title = "Plot of simulation result without relativeDelta for posterior predictive probability", fig = result1)
-  vdiffr::expect_doppelganger(title = "Plot of simulation result with relativeDelta for posterior predictive probability", fig = result2)
+  vdiffr::expect_doppelganger(
+    title = "Plot of simulation result without relativeDelta for posterior predictive probability",
+    fig = result1)
+  vdiffr::expect_doppelganger(
+    title = "Plot of simulation result with relativeDelta for posterior predictive probability",
+    fig = result2)
 })
