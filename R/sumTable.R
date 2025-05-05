106c42f598e6e295a83f5d55f80f0d9d221940d2
Audrey Yeo <comptesaudrey@gmail.com>
2025-05-05 19:41:04 +0200

    clean

--- R/sumTable.R
+++ R/sumTable.R
@@ -1,25 +1,27 @@
-##' Return summary statistics under one response scenario
-##'
-##' This function will calculate the summary statistics for a specific response
-##' outcome scenario.
-##'
-##' @param thisResp number of responses
-##' @param TotalSample sample size
-##' @param cut_B a meaningful improvement threshold: at least cut_B (e.g. 15%) improvement
-##' @param cut_W a poor improvement threshold: at most cut_W (e.g. 5%) improvement
-##' @param parX non-negative parameters of the beta distribution of the control (posterior)
-##' @param YPri non-negative parameters of the beta prior of the treatment, default Beta(0.5,0.5)
-##' @param Round Rounding of the output statistics
-##'
-##' @return A vector with the results.
-##'
-##' @example examples/sumTable.R
-##' @export
+#' Return summary statistics under one response scenario
+#'
+#' This function will calculate the summary statistics for a specific response
+#' outcome scenario.
+#'
+#' @inheritParams sumBetaDiff
+#' @typed thisResp : number
+#'  number of responses
+#' @typed TotalSample : number
+#'  sample size
+#' @typed YPri : numeric
+#'  non-negative parameters of the beta prior of the treatment, default Beta(0.5,0.5)
+#' @typed Round : number
+#' Rounding of the output statistics
+#'
+#' @return A vector with the results.
+#'
+#' @example examples/sumTable.R
+#' @export
 sumTable <- function(thisResp, # number of responses;
                      TotalSample, # Sample size;
                      go_cut, # meaningful improvement: at least cut_B (say 15\%) improvement;
                      stop_cut, # poor improvement: at most cut_W (say 5\%) improvement;
-                     parX, # Two parameters of the beta distribution of the control (posterior);
+                     parX, # Two typedeters of the beta distribution of the control (posterior);
                      YPri = c(0.5, 0.5), # Prior of phase Ib trial, default Beta(0.5,0.5)
                      Round = 2) {
   tmp <- sumBetaDiff(
