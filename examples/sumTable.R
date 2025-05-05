c6cf642c93357d7b02a01bea3bf9dfa89a5d7f06
Audrey Yeo <comptesaudrey@gmail.com>
2025-05-05 23:02:00 +0200

    clean

--- examples/sumTable.R
+++ examples/sumTable.R
@@ -1,4 +1,4 @@
-sumTable(2, 25, parX = c(1, 52), go_cut = 0.2, go_stop = 0.05, YPri = c(1, 1))
+sumTable(2, 25, parX = c(1, 52), go_cut = 0.2, stop_cut = 0.05, YPri = c(1, 1))
 
 # for multiple response scenarios (e.g. 0 to 8 responses out of 25)
 summaries <- do.call(
