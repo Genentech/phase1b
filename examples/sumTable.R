dcc4d67e5a87dddd79471f3e0fdf29021caeeb34
Audrey Yeo <comptesaudrey@gmail.com>
2025-05-07 17:31:07 +0200

    101 sum table (#133)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    Co-authored-by: 27856297+dependabot-preview[bot]@users.noreply.github.com <27856297+dependabot-preview[bot]@users.noreply.github.com>

--- examples/sumTable.R
+++ examples/sumTable.R
@@ -1,11 +1,18 @@
-sumTable(2, 25, parX = c(1, 52), go_cut = 0.2, stop_cut = 0.05, YPri = c(1, 1))
+sumTable(
+  x = 2,
+  n = 25,
+  parX = c(1, 52),
+  parY = c(1, 1),
+  go_cut = 0.2,
+  stop_cut = 0.05
+)
 
 # for multiple response scenarios (e.g. 0 to 8 responses out of 25)
 summaries <- do.call(
   cbind,
   lapply(c(0:8),
     sumTable,
-    TotalSample = 25,
+    n = 25,
     parX = c(1, 52),
     go_cut = 0.2,
     stop_cut = 0.05
