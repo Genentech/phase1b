0cfaf400d709ada53d7286bb548602ce3cd3f2a8
Audrey Yeo <comptesaudrey@gmail.com>
2025-05-07 17:31:07 +0200

    101 sum table (#133)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    Co-authored-by: 27856297+dependabot-preview[bot]@users.noreply.github.com <27856297+dependabot-preview[bot]@users.noreply.github.com>

--- R/sumTable.R
+++ R/sumTable.R
@@ -3,53 +3,59 @@
 #' This function will calculate the summary statistics for a specific response
 #' outcome scenario.
 #'
+#' @inheritParams postprob
 #' @inheritParams sumBetaDiff
-#' @typed thisResp : number
-#'  number of responses
-#' @typed TotalSample : number
-#'  sample size
-#' @typed YPri : numeric
-#'  non-negative parameters of the beta prior of the treatment, default Beta(0.5,0.5)
-#' @typed Round : number
-#' Rounding of the output statistics
+#' @typed round : number
+#' Digit rounding of the output statistics
 #'
 #' @return A vector with the results.
 #'
 #' @example examples/sumTable.R
 #' @export
-sumTable <- function(thisResp, # number of responses;
-                     TotalSample, # Sample size;
-                     go_cut, # meaningful improvement: at least cut_B (say 15\%) improvement;
-                     stop_cut, # poor improvement: at most cut_W (say 5\%) improvement;
-                     parX, # Two typedeters of the beta distribution of the control (posterior);
-                     YPri = c(0.5, 0.5), # Prior of phase Ib trial, default Beta(0.5,0.5)
-                     Round = 2) {
+sumTable <- function(x,
+                     n,
+                     go_cut,
+                     stop_cut,
+                     parX,
+                     parY = c(0.5, 0.5),
+                     round = 2) {
+  assert_numeric(x, lower = 0, upper = n, finite = TRUE)
+  assert_number(n, lower = 0, finite = TRUE)
+  assert_number(go_cut, finite = TRUE)
+  assert_number(stop_cut, finite = TRUE)
+  assert_numeric(parY, len = 2, lower = .Machine$double.xmin, any.missing = FALSE, finite = TRUE)
+  assert_numeric(parX, len = 2, lower = .Machine$double.xmin, any.missing = FALSE, finite = TRUE)
+  assert_count(round)
+
   tmp <- sumBetaDiff(
     parX = parX,
     parY =
       c(
-        thisResp + YPri[1],
-        TotalSample - thisResp + YPri[2]
+        x + parY[1],
+        n - x + parY[2]
       ),
     go_cut = go_cut,
     stop_cut = stop_cut
   )
-
   summaries <- round(c(
-    thisResp,
-    thisResp / TotalSample * 100,
+    x,
+    x / n * 100,
     tmp$mode * 100,
     tmp$ci * 100,
     tmp$go * 100,
     tmp$stop * 100
-  ), Round)
+  ), round)
 
   summaries <- as.data.frame(summaries)
 
   rownames(summaries) <- c(
-    "# resp", "obs ORR [%]", "mode [%]",
-    "CI lower [%]", "CI upper [%]", "prob.go [%]", "prob.nogo [%]"
+    "responders",
+    "obs ORR [%]",
+    "mode [%]",
+    "CI lower [%]",
+    "CI upper [%]",
+    "prob.go [%]",
+    "prob.nogo [%]"
   )
-
-  return(summaries)
+  summaries
 }
