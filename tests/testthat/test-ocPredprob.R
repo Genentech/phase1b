110d7ef4e3d6bf93d9694c8f4a7ea499f2b5cecd
Audrey Yeo <comptesaudrey@gmail.com>
2025-09-24 15:05:42 +0200

    new .svgs

--- tests/testthat/test-ocPredprob.R
+++ tests/testthat/test-ocPredprob.R
@@ -49,85 +49,100 @@ test_that("h_get_decision_two_predprob gives correct result and list", {
 # oc_Predprob ----
 test_that("the sum of Eff, Fut, Gray zone probability is 1", {
   set.seed(1989)
-  expect_warning(results <- ocPredprob(
-    nnE = c(10, 20),
-    truep = 0.4,
-    p0 = 0.25,
-    tT = 0.6,
-    phiL = 0.2,
-    phiU = 0.8,
-    parE = c(1, 1),
-    sim = 50,
-    wiggle = FALSE,
-    decision1 = TRUE
-  ), "Advise to use sim >= 50000 to achieve convergence")
+  expect_warning(
+    results <- ocPredprob(
+      nnE = c(10, 20),
+      truep = 0.4,
+      p0 = 0.25,
+      tT = 0.6,
+      phiL = 0.2,
+      phiU = 0.8,
+      parE = c(1, 1),
+      sim = 50,
+      wiggle = FALSE,
+      decision1 = TRUE
+    ),
+    "Advise to use sim >= 50000 to achieve convergence"
+  )
   results <- sum(results$oc[5:7])
   expect_equal(results, 1)
 })
 
 test_that("the PrFutility increases with increase futility looks", {
   set.seed(1989)
-  expect_warning(result_one_fut <- ocPredprob(
-    nnE = c(10, 20),
-    truep = 0.4,
-    p0 = 0.25,
-    tT = 0.6,
-    phiL = 0.2,
-    phiU = 0.8,
-    parE = c(1, 1),
-    sim = 50,
-    nnF = 10,
-    wiggle = FALSE,
-    decision1 = TRUE
-  ), "Advise to use sim >= 50000 to achieve convergence")
-  expect_warning(result_three_fut <- ocPredprob(
-    nnE = c(10, 20),
-    truep = 0.4,
-    p0 = 0.25,
-    tT = 0.6,
-    phiL = 0.2,
-    phiU = 0.8,
-    parE = c(1, 1),
-    sim = 50,
-    nnF = c(10, 20),
-    wiggle = FALSE,
-    decision1 = TRUE
-  ), "Advise to use sim >= 50000 to achieve convergence")
+  expect_warning(
+    result_one_fut <- ocPredprob(
+      nnE = c(10, 20),
+      truep = 0.4,
+      p0 = 0.25,
+      tT = 0.6,
+      phiL = 0.2,
+      phiU = 0.8,
+      parE = c(1, 1),
+      sim = 50,
+      nnF = 10,
+      wiggle = FALSE,
+      decision1 = TRUE
+    ),
+    "Advise to use sim >= 50000 to achieve convergence"
+  )
+  expect_warning(
+    result_three_fut <- ocPredprob(
+      nnE = c(10, 20),
+      truep = 0.4,
+      p0 = 0.25,
+      tT = 0.6,
+      phiL = 0.2,
+      phiU = 0.8,
+      parE = c(1, 1),
+      sim = 50,
+      nnF = c(10, 20),
+      wiggle = FALSE,
+      decision1 = TRUE
+    ),
+    "Advise to use sim >= 50000 to achieve convergence"
+  )
   expect_true(result_three_fut$oc$PrFutility > result_one_fut$oc$PrFutility)
 })
 
 test_that("the PrEfficacy increases with increase Efficacy looks", {
   set.seed(1989)
-  expect_warning(result_eff <- ocPredprob(
-    nnE = 5,
-    truep = 0.7,
-    p0 = 0.2,
-    p1 = 0.2,
-    tT = 0.6,
-    tF = 0.4,
-    phiU = 0.2,
-    phiFu = 0.8,
-    parE = c(1, 1),
-    sim = 500,
-    wiggle = FALSE,
-    nnF = c(10, 20),
-    decision1 = FALSE
-  ), "Advise to use sim >= 50000 to achieve convergence")
-  expect_warning(result_more_eff <- ocPredprob(
-    nnE = c(5, 20, 40),
-    truep = 0.7,
-    p0 = 0.2,
-    p1 = 0.2,
-    tT = 0.6,
-    tF = 0.4,
-    phiU = 0.2,
-    phiFu = 0.8,
-    parE = c(1, 1),
-    sim = 500,
-    wiggle = FALSE,
-    nnF = c(10, 20),
-    decision1 = FALSE
-  ), "Advise to use sim >= 50000 to achieve convergence")
+  expect_warning(
+    result_eff <- ocPredprob(
+      nnE = 5,
+      truep = 0.7,
+      p0 = 0.2,
+      p1 = 0.2,
+      tT = 0.6,
+      tF = 0.4,
+      phiU = 0.2,
+      phiFu = 0.8,
+      parE = c(1, 1),
+      sim = 500,
+      wiggle = FALSE,
+      nnF = c(10, 20),
+      decision1 = FALSE
+    ),
+    "Advise to use sim >= 50000 to achieve convergence"
+  )
+  expect_warning(
+    result_more_eff <- ocPredprob(
+      nnE = c(5, 20, 40),
+      truep = 0.7,
+      p0 = 0.2,
+      p1 = 0.2,
+      tT = 0.6,
+      tF = 0.4,
+      phiU = 0.2,
+      phiFu = 0.8,
+      parE = c(1, 1),
+      sim = 500,
+      wiggle = FALSE,
+      nnF = c(10, 20),
+      decision1 = FALSE
+    ),
+    "Advise to use sim >= 50000 to achieve convergence"
+  )
   expect_true(result_more_eff$oc$PrEfficacy > result_eff$oc$PrEfficacy)
 })
 
@@ -143,7 +158,7 @@ test_that("ocPredprob correctly shows maximum sample size when no decision reach
     tF = 0.5,
     phiFu = 0.9,
     parE = c(2 / 6, 1 - 2 / 6),
-    sim = 100,
+    sim = 50000,
     wiggle = FALSE,
     decision1 = FALSE
   )
